8281af9508cee80bd55bcb95d5d1898f595fc21a
syber <ninefoldcomplex@gmail.com>
2019-02-25 23:58:30 +0300

    wow app starts and tests work

--- src/main/java/ru/ninefoldcomplex/align/business/config/JpaConfig.java
+++ src/main/java/ru/ninefoldcomplex/align/business/config/JpaConfig.java
@@ -1,6 +1,5 @@
 package ru.ninefoldcomplex.align.business.config;
 
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,8 +7,6 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.hibernate5.HibernateTemplate;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
--- src/test/java/ru/ninefoldcomplex/align/business/config/BeanConfig.java
+++ src/test/java/ru/ninefoldcomplex/align/business/config/BeanConfig.java
@@ -2,7 +2,6 @@ package ru.ninefoldcomplex.align.business.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import ru.ninefoldcomplex.align.business.dao.DAO;
 import ru.ninefoldcomplex.align.business.dao.IDAO;
 import ru.ninefoldcomplex.align.business.service.IService;
@@ -10,13 +9,13 @@ import ru.ninefoldcomplex.align.business.service.ProductService;
 
 @Configuration
 public class BeanConfig {
-//    @Bean
-//    public IDAO iDao() {
-//        return new DAO();
-//    }
-//
-//    @Bean
-//    public IService iService() {
-//        return new ProductService();
-//    }
+    @Bean
+    public IDAO iDao() {
+        return new DAO();
+    }
+
+    @Bean
+    public IService iService() {
+        return new ProductService();
+    }
 }
