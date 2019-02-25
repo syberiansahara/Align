9968eb54c2bbecf681d97cd8895ceba9931b348c
Белялова Марина Сергеевна <ninefoldcomplex@gmail.com>
2019-02-25 16:01:51 +0300

    started with controller

--- src/main/java/ru/ninefoldcomplex/align/business/controller/ProductController.java
+++ src/main/java/ru/ninefoldcomplex/align/business/controller/ProductController.java
@@ -1,7 +1,25 @@
 package ru.ninefoldcomplex.align.business.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.ninefoldcomplex.align.business.service.IService;
+import ru.ninefoldcomplex.align.business.utils.exceptions.ProductNotFoundException;
 
 @RestController
 public class ProductController {
+    @Autowired
+    private IService productService;
+
+    @DeleteMapping()
+    public ResponseEntity<?> removeProduct(Long productId) {
+        try {
+            productService.removeProduct(productId);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (ProductNotFoundException ex) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
